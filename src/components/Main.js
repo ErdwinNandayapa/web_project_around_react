4c0da867f3fbec8fc6f7cacd72e843178bb46eba
Erdwin Nandayapa Cordero <nandayapaerdwin956@gmail.com>
2024-03-30 01:59:39 -0600

    Add loading spinner and loading state to user profile

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,16 +11,21 @@ function Main({
   cards,
   onCardLike,
   onCardDelete,
+  onLogin,
 }) {
   const { currentUser } = useContext(CurrentUserContext);
 
-  return (
+  return onLogin ? (
+    <div className="spinner"></div>
+  ) : currentUser.loading ? (
+    <div className="cargando">Cargando perfil del usuario...</div>
+  ) : (
     <main className="container">
       <section className="profile">
         <div className="profile__overlay">
           <img
             className="profile__avatar"
-            src={currentUser?.avatar}
+            src={currentUser.avatar}
             alt="Avatar"
           />{" "}
           <button
@@ -30,13 +35,13 @@ function Main({
         </div>
         <div className="profile__edit">
           <div className="profile__container">
-            <h1 className="profile__name">{currentUser?.name}</h1>{" "}
+            <h1 className="profile__name">{currentUser.name}</h1>{" "}
             <button
               className="profile__button-edit"
               onClick={onEditProfileClick}
             ></button>
           </div>
-          <p className="profile__profession">{currentUser?.about}</p>{" "}
+          <p className="profile__profession">{currentUser.about}</p>{" "}
         </div>
         <button
           className="profile__button-add"
